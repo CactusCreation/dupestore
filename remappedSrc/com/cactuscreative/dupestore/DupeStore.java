1f3a714f5655ce1a1859a67f64f484416efb73ee
CactusCreation <aiden.harumi@icloud.com>
2026-08-10 12:13:57 -0500

    removed remap

--- remappedSrc/com/cactuscreative/dupestore/DupeStore.java
+++
@@ -1,7 +0,0 @@
-package com.cactuscreative.dupedocs;
-
-// Because this mod is entirely clientside, this class is simply for shared variables or methods.
-
-public class DupeStore {
-	public static final String MOD_ID = "dupedocs";
-}
